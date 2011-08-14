554a5f602dc9ba3b3dc885f8d7c22e440a7fe1e4
Ziad Hatahet <hatahet@gmail.com>
2011-08-14 05:07:18 -0700

    Added several classes to utils.h/.cc -- proper commit now.

--- utils.cc
+++ utils.cc
@@ -7,7 +7,69 @@ namespace raytracer {
 Camera::Camera(const Vector3& position, const Vector3& look_at)
     : pos(position),
       forward((look_at - pos).Normal()),
-       right(1.5 * (forward.Cross(Vector3(0, -1, 0))).Normal()),
-       up(1.5 * (forward.Cross(right)).Normal()) { }
+      right(1.5 * (forward.Cross(Vector3(0, -1, 0))).Normal()),
+      up(1.5 * (forward.Cross(right)).Normal()) { }
+
+
+// Sphere
+
+bool Sphere::Intersect(const Ray& ray, ISect* isect) const {
+  Vector3 eo(center - ray.start);
+  double v = eo.Dot(ray.dir);
+  double dist;
+  if (v < 0) {
+    dist = 0.0;
+  } else {
+    double disc = radius*radius - (eo.Dot(eo) - v*v);
+    dist = disc < 0.0 ? 0.0 : v - std::sqrt(disc);
+  }
+
+  if (dist == 0.0)
+    return false;
+
+  isect->thing = this;  // Careful of potential segfaults. If *this is
+                        // is stack-allocated it must be in the same scope
+                        // as |isect|.  Possible improvement is to do away with
+                        // thing ptr and have a member with only required
+                        // properties of object being intersected copied over.
+  isect->ray = ray;
+  isect->dist = dist;
+  return true;
+}
+
+Vector3 Sphere::Normal(const Vector3& pos) const {
+  return (pos - center).Normal();
+}
+
+
+// Plane
+
+bool Plane::Intersect(const Ray& ray, ISect* isect) const {
+  double denominator = normal.Dot(ray.dir);
+  if (denominator >= 0.0)
+    return false;
+
+  isect->thing = this;
+  isect->ray = ray;
+  isect->dist = (normal.Dot(ray.start) + offset) / (-denominator);
+  return true;
+}
+
+Vector3 Plane::Normal(const Vector3& pos) const {
+  return normal;
+}
+
+
+// Scene
+
+std::vector<ISect> Scene::Intersect(const Ray& ray) const {
+  std::vector<ISect> ret;  // XXX: ISect*
+  ISect isect;
+  for (auto it = things.cbegin(); it != things.cend(); ++it) {
+    if ((*it)->Intersect(ray, &isect))
+      ret.push_back(isect);
+  }
+  return ret;
+}
 
 }  // namesace raytracer
--- utils.h
+++ utils.h
@@ -1,14 +1,105 @@
-#ifndef UTIL_H_
-#define UTIL_H_
+#ifndef UTILS_H_
+#define UTILS_H_
 #pragma once
+
+#include <functional>
+#include <vector>
+
+#include "color.h"
+#include "vector.h"
 
 namespace raytracer {
 
 struct Ray {
+  Ray();
+
   Vector3 start;
   Vector3 dir;
 };
 
-}
+Ray::Ray() : start(Vector3(0, 0, 0)), dir(Vector3(0, 0, 0)) { }
 
-#endif  // UTIL_H_
+struct Light {
+  Vector3 pos;
+  Color color;
+};
+
+class Camera {
+ public:
+  Camera(const Vector3& pos, const Vector3& look_at);
+
+  Vector3 pos;
+  Vector3 forward;
+  Vector3 right;
+  Vector3 up;
+};
+
+struct Surface {
+  std::function<Vector3(Color)> Diffuse;
+  std::function<Vector3(Color)> Specular;
+  std::function<Vector3(double)> Reflect;
+  double roughness;
+};
+
+class SceneObject;
+
+struct ISect {
+  const SceneObject* thing;
+  Ray ray;
+  double dist;
+};
+
+class SceneObject {
+ public:
+    virtual bool Intersect(const Ray& ray, ISect* isect) const = 0;
+    virtual Vector3 Normal(const Vector3& pos) const = 0;
+
+    Surface surface;
+};
+
+class Sphere : public SceneObject {
+public:
+  Sphere(const Vector3& center, double radius);
+  ~Sphere();
+
+  bool Intersect(const Ray& ray, ISect* isect) const override;
+  Vector3 Normal(const Vector3& pos) const override;
+
+  Vector3 center;
+  double radius;
+};
+
+Sphere::Sphere(const Vector3& c, double r)
+    : center(c), radius(r) { }
+
+Sphere::~Sphere() { }
+
+class Plane : public SceneObject {
+ public:
+  Plane(const Vector3& normal, double offset);
+  ~Plane();
+
+  bool Intersect(const Ray& ray, ISect* isect) const override;
+  Vector3 Normal(const Vector3& pos) const override;
+
+  Vector3 normal;
+  double offset;
+};
+
+Plane::Plane(const Vector3& n, double o)
+    : normal(n), offset(o) { }
+
+Plane::~Plane() { }
+
+class Scene {
+ public:
+  std::vector<ISect> Intersect(const Ray& ray) const;  // XXX: change to ISect*?
+
+  std::vector<SceneObject*> things;
+  std::vector<Light> lights;
+  Camera camera;
+};
+
+}  // namespace raytracer
+
+#endif  // UTILS_H_
